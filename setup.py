e7c11c8cf5bf96e6e828c6d20c713aa9f2c73613
AUlyantsev <it.bumerang@gmail.com>
2018-09-16 04:20:07 +0300

    added setup.py for installing as modules with entry points at console

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='db_requester',
+    version='0.1.0',
+    description='simple of test application for running SQL requestings on some DBMS',
+    author='Ulyantsev Aleksandr',
+    author_email='it.bumerang@gmail.com',
+    url='https://github.com/Bumerang47/DBRequesterPy3Qt5',
+    license='GPL-2',
+    long_description=open('README.md').read(),
+    install_requires=open('requirements.txt').read().splitlines(),
+    extras_require={
+      'dev':  [
+          'flake8'
+      ]
+    },
+    entry_points={
+        'console_scripts': [
+            'db_requester=db_requester.main:run_application'
+        ]
+    }
+)
